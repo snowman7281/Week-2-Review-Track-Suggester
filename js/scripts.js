361d325c1d7c4bd06e1fc146abbac625aa846fe7
nv <nv781@yahoo.com>
2017-08-11 10:56:02 -0700

    add functions and branching to connect front-end and back-end.

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,46 @@
 $(document).ready(function(){
   $(".survey").submit(function(event){
-  var seasonInput = parseInt($("#season").val());
-  var animalInput = parseInt($("#animal").val());
-  var foodInput = parseInt($("#food").val());
-  var colorInput = parseInt($("#color").val());
+  var q1Input = parseInt($("input:radio[name=q1]:checked").val());
+  var q2Input = parseInt($("input:radio[name=q2]:checked").val());
+  var q3Input = parseInt($("input:radio[name=q3]:checked").val());
+  var q4Input = parseInt($("input:radio[name=q4]:checked").val());
+  var q5Input = parseInt($("input:radio[name=q5]:checked").val());
 
-  var result = seasonInput + animalInput + foodInput + colorInput;
+  var result = q1Input + q2Input + q3Input + q4Input + q5Input;
 
-    if (result <= 4){
-      $("#css").show();
-      $("#csharp").hide();
-      $("#php").hide();
-      $("#java").hide();
-      $("#rr").hide();
-    } else if (result > 4 && result <= 8){
-      $("#csharp").show();
-      $("#css").hide();
-      $("#php").hide();
-      $("#java").hide();
-      $("#rr").hide();
-    }else if (result > 8 && result <= 12){
-      $("#php").show();
-      $("#csharp").hide();
-      $("#css").hide();
-      $("#java").hide();
-      $("#rr").hide();
-    }else if (result > 12){
-      $("#java").show();
-      $("#csharp").hide();
-      $("#php").hide();
-      $("#css").hide();
-      $("#rr").hide();
-    }else if (result > 12){
-      $("#rr").show();
-      $("#csharp").hide();
-      $("#php").hide();
-      $("#java").hide();
-      $("#css").hide();
-    }else{
-      $("span").show();
-    }
+  if (result <= 5){
+    $("#css").show();
+    $("#csharp").hide();
+    $("#php").hide();
+    $("#java").hide();
+    $("#rr").hide();
+  } else if (result > 5 && result <= 10){
+    $("#csharp").show();
+    $("#css").hide();
+    $("#php").hide();
+    $("#java").hide();
+    $("#rr").hide();
+  }else if (result > 10 && result <= 15){
+    $("#php").show();
+    $("#csharp").hide();
+    $("#css").hide();
+    $("#java").hide();
+    $("#rr").hide();
+  }else if (result > 15 && result <= 20){
+    $("#java").show();
+    $("#csharp").hide();
+    $("#php").hide();
+    $("#css").hide();
+    $("#rr").hide();
+  }else if (result > 20){
+    $("#rr").show();
+    $("#csharp").hide();
+    $("#php").hide();
+    $("#java").hide();
+    $("#css").hide();
+  }else{
+    $("span").show();
+  }
 
     event.preventDefault();
   });
